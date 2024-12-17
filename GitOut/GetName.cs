7117da941354676aab312fe93af5a41bbe6e1212
BenSiener <baconisonlyforme@gmail.com>
2024-12-16 22:21:38 -0500

    Added GetName

---
+++ GitOut/GetName.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOut
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GetName : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Ben Siener" });
+        }
+    }
+}
